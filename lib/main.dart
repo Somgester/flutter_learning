31e5b7d338437b97431cbcefb2773d91b3c30392
Somgester <dhruvratmele13@gmail.com>
2024-10-05 21:21:30 +0530

    pushed

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Anshraj Nalla'),
+      home: const MyHomePage(title: 'Basic Counter'),
     );
   }
 }
